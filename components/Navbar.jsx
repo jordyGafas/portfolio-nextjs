4d7c28fe1e1ce77930679c0a3345d33cff7b26fe
jordyGafas <39955874+jordyGafas@users.noreply.github.com>
2024-05-25 15:16:22 +0200

    Update resume link

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
               <Link href="/#projects">Projects</Link>
             </li>
             <li className="ml-10 text-sm uppercase hover:border-b">
-              <Link href="/resume">Resume</Link>
+              <Link target="_blank" href="/resume.pdf">Resume</Link>
             </li>
             <li className="ml-10 text-sm uppercase hover:border-b">
               <Link href="/#contact">Contact</Link>
@@ -136,7 +136,7 @@ const Navbar = () => {
                   Projects
                 </li>
               </Link>
-              <Link href="/resume">
+              <Link target="_blank" href="/resume.pdf">
                 <li onClick={() => setNav(false)} className="py-4 text-sm">
                   Resume
                 </li>
