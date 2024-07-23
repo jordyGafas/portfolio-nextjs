1b7ce70603d66c3edc76cdf3d1350ee21a2a0d34
jordyGafas <39955874+jordyGafas@users.noreply.github.com>
2024-07-23 13:01:28 +0200

    Add GA

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
-      <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0SPZNYFBFK"></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-0SPZNYFBFK');`
+            }
+        </script>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
